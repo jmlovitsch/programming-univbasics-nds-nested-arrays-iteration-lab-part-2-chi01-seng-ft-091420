7fadd67bfd1b1f05e27edf6f1c0f6cb51977ac0f
John Lovitsch <jmlovitsch@gmail.com>
2020-09-02 17:32:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,12 +3,12 @@ def find_min_in_nested_arrays(array_of_daily_temperatures)
   while row_index < array_of_daily_temperatures.count do
     element_index = 0 
     min_value = 10000
-    while element_index < array_of_daily_temperatures[count].length do
-      if min_value > array_of_daily_temperatures[count][inner_count]
-        min_value = array_of_daily_temperatures[count][inner_count]
+    while element_index < array_of_daily_temperatures[row_index].count do
+      if min_value > array_of_daily_temperatures[row_index][element_index]
+        min_value = array_of_daily_temperatures[row_index][element_index]
       end
     end
-   inner_count += 1  
+   element_index += 1  
   end
-  count += 1
+  row_index += 1
 end
